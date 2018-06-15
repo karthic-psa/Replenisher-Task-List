a6a8cc5fd84b49e161f0438f19bbdf588e4b22da
Srinivasa Karthic Ponguru <karthic.psa@gmail.com>
2018-06-15 15:59:54 -0700

    feat: added task lists view and staff auth

--- tasklistapp/views.py
+++ tasklistapp/views.py
@@ -1,5 +1,64 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-from django.shortcuts import render,redirect
+import datetime
 
-# Create your views here.
+from django.conf import settings
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.sites.models import Site
+from django.core.exceptions import PermissionDenied
+from django.core.mail import send_mail
+from django.db import IntegrityError
+from django.db.models import Q
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render, redirect
+from django.template.loader import render_to_string
+from django.urls import reverse
+from django.utils import timezone
+from django.utils.text import slugify
+from django.views.decorators.csrf import csrf_exempt
+
+from models import Task, TaskList
+
+
+def staff_only(function):
+    def wrap(request, *args, **kwargs):
+        if request.user.is_staff:
+            return function(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
+
+    wrap.__doc__ = function.__doc__
+    wrap.__name__ = function.__name__
+    return wrap
+
+@login_required
+def task_lists(request):
+    tdate = datetime.datetime.now()
+
+    # Make sure user belongs to at least one group.
+    if request.user.groups.all().count() == 0:
+        messages.warning(request, "You do not yet belong to any groups. Ask your administrator to add you to one.")
+
+    # Superusers see all lists
+    if request.user.is_superuser:
+        lists = TaskList.objects.all().order_by('group', 'name')
+    else:
+        lists = TaskList.objects.filter(group__in=request.user.groups.all()).order_by('group', 'name')
+
+    cnt_list = lists.count()
+
+    # superusers see all lists, so count shouldn't filter by just lists the admin belongs to
+    if request.user.is_superuser:
+        cnt_task = Task.objects.filter(completed=0).count()
+    else:
+        cnt_task = Task.objects.filter(completed=0).filter(task_list__group__in=request.user.groups.all()).count()
+
+    context = {
+       "lists": lists,
+       "tdate": tdate,
+        "cnt_task": cnt_task,
+       "cnt_list": cnt_list,
+    }
+
+    return render(request, 'index.html', context)
